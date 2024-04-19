d5aa491ed7dda53208c4020ed86ed8b1f8bae133
Daviddairot <133882462+Daviddairot@users.noreply.github.com>
2024-04-19 21:01:49 +0100

    Update wsgi.py

--- finialproject/wsgi.py
+++ finialproject/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finialproject.settings')
 
 application = get_wsgi_application()
